6e32d625a4102b4e015727ee239087f2024b22d0
schandaluri <schandaluri@nisum.com>
2020-04-30 15:18:56 -0700

    POC-000 [Sai Dinesh] Removing the search flow

--- src/main/java/com/employee/expertise/controller/ExpertiseController.java
+++ src/main/java/com/employee/expertise/controller/ExpertiseController.java
@@ -4,22 +4,16 @@ import com.employee.expertise.dao.Candidate;
 import com.employee.expertise.domain.ExpertsPerDomain;
 import com.employee.expertise.service.ExpertConsolidationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 
 
 /**
  * @author Sai Dinesh Chandaluri
  */
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
 public class ExpertiseController {
 
     @Autowired
@@ -38,10 +32,5 @@ public class ExpertiseController {
     public String addExperts(@RequestBody Candidate candidate) {
         consolidationService.addExpert(candidate);
         return "Successfully added candidate and his expertise ";
-    }
-
-    @GetMapping("/search")
-    public String addExperts(@RequestParam String key) {
-        return "";
     }
 }
